9e049dcf44bf149eb3c6bf64678c1f82c55d4309
Aditya-AG7 <147618452+Aditya-AG7@users.noreply.github.com>
2024-12-19 11:59:55 +0530

    Update constants.dart

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,31 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 /// Colors
 const kColor1 = Color(0xff343F5D); // Primary Color
 const kColor2 = Color(0xffE25041); // Color of 'X'
 const kColor3 = Color(0xff1BBC9B); // Color of 'O'
-const kShadowColor = Color(0xff222A41); // Shadow and border color
+const kShadowColor = Color(0xff222A41); // Shadow and button color
 
 /// Button Style
 ButtonStyle kButtonStyle = ElevatedButton.styleFrom(
-  padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
-  backgroundColor: kColor1,
+  padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+  backgroundColor: kShadowColor,
   elevation: 10,
-  shadowColor: kShadowColor,
+  shadowColor: Colors.white38,
   shape: RoundedRectangleBorder(
     borderRadius: BorderRadius.circular(15),
   ),
   side: const BorderSide(
-    color: Color(0xff222A41),
-    width: 2,
-    strokeAlign: 5,
+    color: kShadowColor,
+    width: 5,
   ),
 );
 
 /// Text Styles
-const kPlayButtonTextStyle = TextStyle(
+TextStyle kPlayButtonTextStyle = GoogleFonts.baloo2(
   color: kColor2,
   fontSize: 50,
-  fontWeight: FontWeight.w900,
   letterSpacing: 3,
+  fontWeight: FontWeight.w900,
 );
